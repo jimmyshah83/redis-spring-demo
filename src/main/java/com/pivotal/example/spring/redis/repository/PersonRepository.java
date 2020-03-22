3f921e73b8f8024650b0d3db1edd6a12c875789f
Jimmy Shah <jimmyshah83@gmail.com>
2020-03-22 10:19:34 -0400

    Delete repository as we are Reactive Value Operations

--- src/main/java/com/pivotal/example/spring/redis/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pivotal.example.spring.redis.repository;
-
-import com.pivotal.example.spring.redis.model.Person;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface PersonRepository extends ReactiveCrudRepository<Person, String> {
-}
